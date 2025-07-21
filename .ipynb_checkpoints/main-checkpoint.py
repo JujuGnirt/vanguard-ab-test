fb658b7113590f39b3f1e2f21215cde7b50da50d
Adam Askari <kitoaskari2003@gmail.com>
2025-07-21 15:08:53 +0100

    tableau 1

---
+++ .ipynb_checkpoints/main-checkpoint.py
@@ -0,0 +1,10 @@
+def main():
+<<<<<<< HEAD
+    print("Hello from week5!")
+=======
+    print("Hello from vanguard-ab-test!")
+>>>>>>> 4531264564a9b87a07a69d194eef4dd4931f1ce0
+
+
+if __name__ == "__main__":
+    main()
